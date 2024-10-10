00c4309d0971bfd73a31549217d53d464fde3d2d
Sebastian Godelet <sebastian.godelet@outlook.com>
2024-10-10 13:50:00 +1100

    Add EAF init support

---
+++ include/EAFFocuser1.6.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ZWOptical.ASISDK
+{
+    public static class EAFFocuser16
+    {
+        [DllImport("EAFFocuser1.6", EntryPoint = "EAFGetNum", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int EAFGetNum();
+    }
+}
